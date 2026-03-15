84a865065dd77eb570be0d8a6c1e6becda3e79e6
Poonam0208 <144873366+Poonam0208@users.noreply.github.com>
2026-03-15 10:24:49 +0530

    first commit

---
+++ assetes/js/app.js
@@ -0,0 +1,133 @@
+let cl = console.log;
+
+
+const PostContainer = document.getElementById('PostContainer');
+const spinner = document.getElementById('spinner')
+const PostForm = document.getElementById('PostForm');
+const titleControl = document.getElementById('title');
+const bodyControl = document.getElementById('body');
+const userIdControl = document.getElementById('userId');
+
+const BASE_URL = `https://jsonplaceholder.typicode.com`;
+const POST_URL = `${BASE_URL}/posts`;
+
+
+
+function SnackBar(msg, icon){
+    Swal.fire({
+        title : msg,
+        icon: icon,
+        timer: 3000
+    })
+}
+
+let postArr ;
+
+
+function CreatePostCard(arr){
+    let result = ``;
+
+    for(let i = arr.length -1; i >=0 ; i--){
+        result += `
+         <div class="col-md-4 mb-4" id="${arr[i].id}">
+            <div class="card h-100">
+                <div class="card-header">
+                    <h5>${arr[i].title}</h5>
+                </div>
+                <div class="card-body">
+                    <p>${arr[i].body}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                      <button onclick ="onEdit(this)" class="btn btn-outline-primary">Edit</button>
+                      <button onclick ="onRemove(this)"  class="btn btn-outline-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+        
+        `
+    }
+    PostContainer.innerHTML = result;
+
+}
+
+function fetchBlog(){
+    spinner.classList.remove('d-none')
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', POST_URL, true);
+
+    xhr.send();
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status < 299){
+            let postArr = JSON.parse(xhr.response)
+             CreatePostCard(postArr)
+             spinner.classList.add('d-none')
+            
+        }else{
+            SnackBar(`Somthing went wrong !!!`, `error`)
+            spinner.classList.add('d-none')
+        }
+    }
+}
+
+fetchBlog()
+
+function onSubmitPost(eve){
+    eve.preventDefault();
+
+    let POST_OBJ = {
+        title : titleControl.value,
+        body : bodyControl.value,
+        userId : userIdControl.value
+    }
+    cl(POST_OBJ)
+    
+
+    spinner.classList.remove('d-none')
+
+    let xhr = new XMLHttpRequest()
+
+    xhr.open("POST", POST_URL, true)
+    xhr.send(JSON.stringify(POST_OBJ));
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status < 299){
+            // cl(xhr.response)
+
+            PostForm.reset();
+            let res = JSON.parse(xhr.response)
+
+            let col = document.createElement('div');
+            col.className = 'col-md-4 mb-4';
+            col.id = res.id;
+
+            col.innerHTML = `
+            <div class="card h-100">
+                <div class="card-header">
+                    <h5>${POST_OBJ.title}</h5>
+                </div>
+                <div class="card-body">
+                    <p>${POST_OBJ.body}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                      <button onclick ="onEdit(this)" class="btn btn-outline-primary">Edit</button>
+                      <button onclick ="onRemove(this)"  class="btn btn-outline-danger">Delete</button>
+                </div>
+            </div>
+            `
+
+            PostContainer.prepend(col)
+             spinner.classList.add('d-none')
+        }else{
+            SnackBar(`Somthing went wrong !!!`, `error`)
+            spinner.classList.add('d-none')
+        }
+    }
+}
+
+
+
+
+PostForm.addEventListener("submit", onSubmitPost)
